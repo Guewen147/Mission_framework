361a90fdd408d4cb7e4ea4647c7969043721533f
YvesQuintin <90259261+YvesQuintin@users.noreply.github.com>
2022-02-08 08:32:29 +0100

    Update produit_marque.php

--- CodeIgniter/App/Views/pages/produit_marque.php
+++ CodeIgniter/App/Views/pages/produit_marque.php
@@ -8,15 +8,27 @@
 </head>
 
 <body>
+    <!-- Formulaire pour la barre de recherche -->
     <form class="search" method="GET">
         <input class="send" type="search" name="q" placeholder="Rechercher un produit">
-        <button type="submit" action="<?php echo base_url('Home/recherche') ?>">Rechercher</button>
+        <button type="submit">Rechercher</button>
     </form>
 
 
-    <h1> Liste des produit par marque </h1>
+   <h1> Liste des produits <?php if (isset($_GET['name'])) { echo $_GET['name']; } ?></h1>
 
-    
+    <?php
+
+        $session = \Config\Services::session();
+
+        if($session->getFlashdata('success'))
+        {
+            echo '
+            <div class="alert alert-success">'.$session->getFlashdata("success").'</div>
+            ';
+        }
+
+        ?>
 
         <div class="container1">
             <ul class="responsive-table">
@@ -26,6 +38,7 @@
                         <form class="listeD" method="GET" action="">
                             <select name="name" id="name">
                                 <option value="">Toutes</option>
+                                <!-- Affiche toutes les marques active dans une liste déroulante -->
                                 <?php foreach ($liste as $déroulante) { ?>
                                     <option> <?php echo $déroulante->name ?></option>
                                 <?php  } ?>
@@ -37,6 +50,7 @@
                     <div class="col col-3">Prix d'achat</div>
                     <div class="col col-3">Prix de Vente</div>
                     <div class="col col-5"> MODIFIER <br><br>
+                        <!-- Affichage du bouton Modifier pour les produits d'un marque si le nam dans l'URL -->
                     <?php if (isset($_GET['name']) and !empty($_GET['name'])) { ?>
         <div class="col col-4" data-label="MODIFIER"><a href="<?php echo base_url('Home/modif_marque') ?>?name_marque=<?= $_GET['name'] ?>"><span  id=button1>Modifier par marques</span id=button> </a>
         <?php } ?>
@@ -47,7 +61,7 @@
                 <?php
 
                 if (!empty($id_data)) {
-
+                    // Affichage de l'id le nom de la marque, le nom du produit, le prix d-achat, le prix de vente pour chaque produit  de la requête
                     foreach ($id_data as $product) { ?>
 
                         <li class="table-row">
@@ -57,6 +71,7 @@
                             <div class="col col-3" data-label="ps_product_lang.name"><?php echo $product->name_pro ?></div>
                             <div class="col col-3" data-label="price"><?php echo   $product->wholesale_price ?></div>
                             <div class="col col-3" data-label="price"><?php echo   $product->price ?></div>
+                            <!-- Redirection Ajout dans l'URL si l'utilisateur clique sur le bouton -->
                             <div class="col col-5" data-label="MODIFIER"><a href="<?php echo base_url('Home/modif_marque') ?>?id_product=<?php echo $product->id_product; ?>&name=<?php echo $product->name_manu; ?>&multiplicateur_value=<?php echo $product->multiplicateur_value; ?>"><span id=button>Modifier</span id=button> </a>
 
                         </li>
